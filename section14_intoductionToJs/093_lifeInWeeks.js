1973cd38351e37a48347c4f6cbd143e19fcbc35a
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-06-07 14:49:37 +0530

    First commit

---
+++ section14_intoductionToJs/093_lifeInWeeks.js
@@ -0,0 +1,15 @@
+function lifeInWeeks(age) {
+    /************Don't change the code above************/    
+        //Write your code here.
+        age = 90 - age;
+        console.log(
+            "You have "
+            + age * 365
+            + " days, "
+            + age * 52
+            + " weeks, and "
+            + age * 12
+            + " months left."
+        );
+    /*************Don't change the code below**********/
+}
