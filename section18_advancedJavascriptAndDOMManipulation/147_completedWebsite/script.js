e6b565c05da3a4f14bed38867e625ed3d9d5f050
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-07-01 18:35:10 +0530

    Learnt switch statements

--- section18_advancedJavascriptAndDOMManipulation/147_completedWebsite/script.js
+++ section18_advancedJavascriptAndDOMManipulation/147_completedWebsite/script.js
@@ -1,9 +1,42 @@
+drumButtons = document.querySelectorAll("button");
 numberOfDrums = document.querySelectorAll(".drum").length;
+
 for (i = 0; i < numberOfDrums; i++) {
-    document.querySelectorAll("button ")[i].addEventListener("click", handleClick);
+    drumButtons[i].addEventListener("click", handleClick);
 }
 
 function handleClick() {
-    tom1 = new Audio("./sounds/tom-1.mp3");
-    tom1.play();
+    buttonInnerHTML = this.innerHTML;
+    switch(buttonInnerHTML) {
+        case "w":
+            tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default:
+            console.log("Unexpected input");
+    }
 }
