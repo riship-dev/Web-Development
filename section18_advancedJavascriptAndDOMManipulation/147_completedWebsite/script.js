62cad2a4b27671f3b67d8fa26c3391f06beec20e
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-07-02 12:16:53 +0530

    Added animations, website complete

--- section18_advancedJavascriptAndDOMManipulation/147_completedWebsite/script.js
+++ section18_advancedJavascriptAndDOMManipulation/147_completedWebsite/script.js
@@ -1,17 +1,19 @@
-// Creating an array of all "drum" buttons.
-drumButtons = document.querySelectorAll(".drum");
-numberOfDrums = document.querySelectorAll(".drum").length;
-// Adding mouse click event listener to all ".drum" buttons.
-for (i = 0; i < numberOfDrums; i++) 
+drumButtons = document.querySelectorAll(".drum"); // Creating an array of all "drum" buttons.
+
+for (i = 0; i < drumButtons.length; i++) // Adding mouse click event listener to all ".drum" buttons.
     drumButtons[i].addEventListener("click", handleClick);
+addEventListener("keydown", handleKey); // Adding key click event listener.
 
-// Adding key click event listener.
-addEventListener("keydown", handleKey);
-
-// Function that handles mouse click behavior.
-function handleClick() {
-    buttonInnerHTML = this.innerHTML;
-    switch(buttonInnerHTML) {
+function handleClick() { // Function that handles mouse click behavior.
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+} 
+function handleKey(event) { // Function that handles key click behavior.
+    makeSound(event.key);
+    buttonAnimation(event.key);
+} 
+function makeSound(currentEvent) { // Function that makes events make sound.
+    switch(currentEvent) {
         case "w":
             tom1 = new Audio("./sounds/tom-1.mp3");
             tom1.play();
@@ -44,39 +46,13 @@ function handleClick() {
             console.log("Unexpected input");
     }
 }
-// Function that handles key click behavior.
-function handleKey(event) {
-    console.log(event.key);
-    switch(event.key) {
-        case "w":
-            tom1 = new Audio("./sounds/tom-1.mp3");
-            tom1.play();
-            break;
-        case "a":
-            tom2 = new Audio("./sounds/tom-2.mp3");
-            tom2.play();
-            break;
-        case "s":
-            tom3 = new Audio("./sounds/tom-3.mp3");
-            tom3.play();
-            break;
-        case "d":
-            tom4 = new Audio("./sounds/tom-4.mp3");
-            tom4.play();
-            break;
-        case "j":
-            snare = new Audio("./sounds/snare.mp3");
-            snare.play();
-            break;
-        case "k":
-            crash = new Audio("./sounds/crash.mp3");
-            crash.play();
-            break;
-        case "l":
-            kick = new Audio("./sounds/kick-bass.mp3");
-            kick.play();
-            break;
-        default:
-            console.log("Unexpected input");
-    }
+function buttonAnimation(currentEvent) {
+    activeButton = document.querySelector("." + currentEvent);
+    activeButton.classList.add("pressed");
+    setTimeout (
+        function() {
+            activeButton.classList.remove("pressed");
+        }, 
+        100
+    );
 }
