6efaf9831bf47012e728f72e74054297fb526b68
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-07-05 17:19:44 +0530

    Step 2

--- section20_bossLevelChallenge2_theSimonGame/183_completedProject/script.js
+++ section20_bossLevelChallenge2_theSimonGame/183_completedProject/script.js
@@ -1,1 +1,8 @@
-alert("Works");
+buttonColours = ["red", "blue", "green", "yellow"];
+gamePattern = [];
+
+function nextSequence() {
+    randomNumber = Math.floor(Math.random() * 4);
+    randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+}
