0dad863a122701da854242e622cfbc3232e09a65
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-07-05 17:21:58 +0530

    Step 3

--- section20_bossLevelChallenge2_theSimonGame/183_completedProject/script.js
+++ section20_bossLevelChallenge2_theSimonGame/183_completedProject/script.js
@@ -5,4 +5,8 @@ function nextSequence() {
     randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
