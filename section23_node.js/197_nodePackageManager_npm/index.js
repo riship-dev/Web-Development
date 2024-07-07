fc55bf39fffad7b514b3e4d63c5abb19f9d951f6
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-07-07 20:24:43 +0530

    First commit

---
+++ section23_node.js/197_nodePackageManager_npm/index.js
@@ -0,0 +1,8 @@
+/*
+import generateName from "sillyname";
+var sillyName = generateName();
+console.log(`My name is ${sillyName}.`);
+*/
+
+import {randomSuperhero} from "superheroes";
+console.log(`I am ${randomSuperhero()}!`);
