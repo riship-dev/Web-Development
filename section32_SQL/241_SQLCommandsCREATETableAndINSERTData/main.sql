5202c0b67af64f4d87c6b166873ea1342fc1e8a4
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-08-25 04:42:26 +0530

    Learn create and insert

---
+++ section32_SQL/241_SQLCommandsCREATETableAndINSERTData/main.sql
@@ -0,0 +1,12 @@
+CREATE TABLE products (
+    id INT NOT NULL,
+    name SQL,
+    price MONEY,
+    PRIMARY KEY (id)
+);
+
+INSERT INSERT INTO products 
+VALUES (1, 'Pen', 1.20);
+
+INSERT INTO products (id, name)
+VALUES (2, 'Pencil'); 
