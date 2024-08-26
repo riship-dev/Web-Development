5630d1c4a3d8715813cc43734e6d6883e0bf32e5
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-08-26 12:43:22 +0530

    Learn create table

---
+++ section33_postgreSQL/251_usePgAdminToCreateATable/capitals.sql
@@ -0,0 +1,7 @@
+CREATE TABLE capitals (
+    id SERIAL PRIMARY KEY,
+	country VARCHAR(45),
+	capital VARCHAR(45)
+);
+
+SELECT * FROM capitals;
---
+++ section33_postgreSQL/251_usePgAdminToCreateATable/flags.sql
@@ -0,0 +1,7 @@
+create table flags (
+    id serial primary key,
+	name varchar(45),
+	flag text
+);
+
+select * from flags;
